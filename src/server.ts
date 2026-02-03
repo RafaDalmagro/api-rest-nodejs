470cf9821f3d1094568426d1719309ff216e6d59
RafaDalmagro <rafa.limadalmagro@gmail.com>
2026-02-03 11:58:48 -0300

    feat: instala e configura o tsx e types/node.

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
 import fastfy from "fastify";
 
 const app = fastfy();
+
+app.get("/hello", () => {
+    return { greeting: "Hello, World!" };
+});
+
+app.listen({ port: 3000 }).then(() => {
+    console.log("Server is running on http://localhost:3000");
+});
